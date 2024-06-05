16f79d9443b311f8886789f595a2e90a34d9cff7
Jack Cotter <jackwcotter101@gmail.com>
2024-06-04 17:44:03 -0700

    add listings endpoints

--- flask/blueprints/__init__.py
+++ flask/blueprints/__init__.py
@@ -1,6 +1,8 @@
 from .auth import auth_bp
 from .users import user_bp
+from .listings import listings_bp
 
 def init_app(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(user_bp, url_prefix='/user')
+    app.register_blueprint(listings_bp, url_prefix='/listings')
---
+++ flask/blueprints/listings.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, jsonify
+
+listings_bp = Blueprint('listings', __name__)
+
+@listings_bp.post('/')
+def create_listing():
+    return {}
+
+@listings_bp.patch('/<int:listing_id>')
+def patch_listing(listing_id):
+    return {}
+
+@listings_bp.delete('/<int:listing_id>')
+def delete_listing(listing_id):
+    return {}
+
+@listings_bp.get('/')
+def get_listings():
+    return {}
+
+@listings_bp.get('/<int:listing_id>')
+def get_listing(listing_id):
+    return {}
+
+@listings_bp.get('/me')
+def my_listings():
+    return {}
