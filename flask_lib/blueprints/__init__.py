048263733932fc774dfe772b021aeebad5063cad
Jack Cotter <jackwcotter101@gmail.com>
2024-06-04 19:58:38 -0700

    recomendations -> recommendations

--- flask_lib/blueprints/__init__.py
+++ flask_lib/blueprints/__init__.py
@@ -4,7 +4,7 @@ from .listings import listings_bp
 from .rating import rating_bp
 from .review import review_bp
 from .search import search_bp
-from .recomendations import recomendations_bp
+from .recommendations import recommendations_bp
 from .chats import chats_bp
 from .messages import messages_bp
 
@@ -15,6 +15,6 @@ def init_app(app):
     app.register_blueprint(rating_bp, url_prefix='/rating')
     app.register_blueprint(review_bp, url_prefix='/review')
     app.register_blueprint(search_bp, url_prefix='/search')
-    app.register_blueprint(recomendations_bp, url_prefix='/recomendations')
+    app.register_blueprint(recommendations_bp, url_prefix='/recommendations')
     app.register_blueprint(chats_bp, url_prefix='/chats')
     app.register_blueprint(messages_bp, url_prefix='/messages')
