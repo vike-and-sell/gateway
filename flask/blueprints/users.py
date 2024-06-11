a7feb98e94e4afd20d460422be73d467f5f0bdf5
Brennan McMicking <brennanmcmicking@gmail.com>
2024-06-11 16:56:21 -0700

    add helper functions, start of get_user endpoint

--- flask/blueprints/users.py
+++ flask/blueprints/users.py
@@ -1,24 +1,36 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 import gateway
 
+import urllib3
+http = urllib3.PoolManager()
+
 users_bp = Blueprint('user', __name__)
+
+
+def make_response(result):
+    return Response(result.get("body"), status=result.get("statusCode"), mimetype="application/json")
+
 
 @users_bp.patch('/<int:user_id>')
 def patch_user(user_id):
     result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    return make_response(result)
+
 
 @users_bp.get('/me')
 def my_user():
     result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    return make_response(result)
+
 
 @users_bp.get('/<int:user_id>')
 def get_user(user_id):
-    result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    auth_token = request.cookies.get("Authorization")
+    result = gateway.get_user_by_id(http, auth_token, user_id)
+    return make_response(result)
+
 
 @users_bp.get('/<int:user_id>/searches')
 def searches_by_user(user_id):
     result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    return make_response(result)
