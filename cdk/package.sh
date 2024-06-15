9c03599b21037b7e096b5a56d64cd5d39ba5e792
Brennan McMicking <brennanmcmicking@gmail.com>
2024-06-14 22:24:42 -0700

    update: requirements.txt and add Lambda layer packaging (untested)

--- cdk/package.sh
+++ cdk/package.sh
@@ -1,4 +1,8 @@
 mkdir -p packaging
+mkdir -p packaging/layer
+
+cp ../flask/requirements.txt packaging/layer/requirements.txt
+
 for f in ../lambda/*.py ; do 
     name=$(basename $f)
     zip -j -r "packaging/${name%.py}.zip" ../gateway.py $f
