ad8070aca828f2a44303dbd6c22884bcf4dee00a
Brennan McMicking <brennanmcmicking@gmail.com>
2024-06-22 15:21:41 -0700

    unfinished account management work

---
+++ tests/test_auth.py
@@ -0,0 +1,11 @@
+from mockito import when, mock, verify
+import urllib3
+import json
+from test_utils import sign_jwt_for_test, DATA_API_KEY, MAPS_API_KEY
+import datetime
+
+import gateway
+
+
+def test_send_email():
+    pass
