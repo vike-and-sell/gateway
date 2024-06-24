eb305849f61f5bf7ea93937b7f53357fdda42d17
jenny <jluu@uvic.ca>
2024-06-23 17:13:34 -0700

    review & users stubs

--- flask/blueprints/review.py
+++ flask/blueprints/review.py
@@ -1,14 +1,20 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 import gateway
+from shared import make_response
+import urllib3
 
 review_bp = Blueprint('review', __name__)
+http = urllib3.PoolManager()
 
 @review_bp.get('/<int:listing_id>')
 def get_review(listing_id):
-    result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    auth_token = request.cookies.get("Authorization")
+    result = gateway.get_reviews_by_listing_id(http, auth_token, listing_id)
+    return make_response(result)
 
 @review_bp.post('/<int:listing_id>')
 def create_review(listing_id):
-    result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    auth_token = request.cookies.get("Authorization")
+    review = request.json
+    result = gateway.post_review_by_listing_id(http, auth_token, listing_id, review)
+    return make_response(result)
--- flask/blueprints/search.py
+++ flask/blueprints/search.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, Response
 import gateway
+from shared import make_response
 
 search_bp = Blueprint('search', __name__)
 
 @search_bp.get('/')
 def get_search(q = None):
     result = gateway.not_implemented()
-    return Response(result["body"], status=result["statusCode"], mimetype="application/json")
+    return make_response(result)
