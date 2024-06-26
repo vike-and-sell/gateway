70bcaf0185c95730bab5b4b12fa5faf7e1be4def
Brennan McMicking <brennanmcmicking@gmail.com>
2024-06-26 11:12:48 -0700

    add: CORS headers on OPTIONS requests

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from blueprints import init_app
 
 app = Flask(__name__)
+app.config["CORS_AUTOMATIC_OPTIONS"] = True
 CORS(app, supports_credentials=True)  # This will enable CORS for all routes
 
 init_app(app)
