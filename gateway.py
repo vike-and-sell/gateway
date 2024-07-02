c49e7ec6d226d38f0aa7cf26e06ae45ed2676fd9
JackCotter <79673714+JackCotter@users.noreply.github.com>
2024-07-02 11:48:39 -0700

    Address to lat long use in create listing (#31)
    * implemented address to lat long use in create listing
    
    * parse incoming address

--- gateway.py
+++ gateway.py
@@ -470,12 +470,22 @@ def create_listing(http: urllib3.PoolManager, auth_token, listing_data):
     if not creds:
         return make_unauthorized_response()
 
+    address = listing_data["address"]
+    parsed_address = parse_address(address)
+
+    if parsed_address is None:
+        print(f"failed to parse address {address}")
+        return make_invalid_request_response("Invalid address")
+
+    full_address = parsed_address.full_address
+    lat, lng = address_to_latlng(http, full_address)
+
     result = execute_data_post(http, f"/create_listing", {
         "sellerId": creds,
         "title": listing_data["title"],
         "price": listing_data["price"],
-        "latitude": listing_data['latitude'],
-        "longitude": listing_data['longitude'],
+        "latitude": lat,
+        "longitude": lng,
         "address": listing_data["address"],
         "status": listing_data["status"],
     })
