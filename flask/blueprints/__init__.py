c159bbc8476f31e7ae2c02fa2b60f68c0feb6c6b
Brennan McMicking <brennanmcmicking@gmail.com>
2024-07-23 20:22:48 -0700

    add logout, fix listing creation and update to match api spec

--- flask/blueprints/__init__.py
+++ flask/blueprints/__init__.py
@@ -8,6 +8,7 @@ from .recommendations import recommendations_bp
 from .chats import chats_bp
 from .messages import messages_bp
 
+
 def init_app(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(users_bp, url_prefix='/users')
