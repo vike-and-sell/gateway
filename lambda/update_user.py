19037865e8784e0a078a00b3672ca5cd3b095913
Brennan McMicking <brennanmcmicking@gmail.com>
2024-07-26 23:28:10 -0700

    fix: lambda address->location field names

--- lambda/update_user.py
+++ lambda/update_user.py
@@ -9,6 +9,6 @@ http = urllib3.PoolManager()
 def handler(event, context):
     auth_token = get_auth_token(event)
     body = get_body(event)
-    address = body.get('address')
+    address = body.get('location')
     result = gateway.update_user(http, auth_token, address)
     return mould_response(result)
