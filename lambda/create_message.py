275a6267c5da2d86070fb1671e29d6651b3f0044
Jack Cotter <jackwcotter101@gmail.com>
2024-07-27 13:44:07 -0700

    move listingId to the path as per API agreement

--- lambda/create_message.py
+++ lambda/create_message.py
@@ -1,4 +1,4 @@
-from shared import mould_response, get_auth_token, get_body
+from shared import mould_response, get_auth_token, get_body, get_path_params
 
 import gateway
 import urllib3
@@ -7,8 +7,9 @@ http = urllib3.PoolManager()
 
 def handler(event, context):
     auth_token = get_auth_token(event)
+    params = get_path_params(event)
+    chat_id = params.get('chatId')
     body = get_body(event)
-    chat_id = body.get('chatId')
     content = body.get('content')
     result = gateway.write_message(http, auth_token, chat_id, content)
     return mould_response(result)
